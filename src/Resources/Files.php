f4586fcb5272f49b95474b77ab41b7de5ccf84c2
Nuno Maduro <enunomaduro@gmail.com>
2022-09-25 23:33:18 +0100

    fix: removes non-implemented API

--- src/Resources/Files.php
+++ src/Resources/Files.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OpenAI\Resources;
 
-use Exception;
 use OpenAI\ValueObjects\Transporter\Payload;
 
 final class Files
@@ -26,49 +25,5 @@ final class Files
         $result = $this->transporter->request($payload);
 
         return $result;
-    }
-
-    /**
-     * Upload a file that contains document(s) to be used across various endpoints/features.
-     *
-     * @see https://beta.openai.com/docs/api-reference/files/upload
-     *
-     * @param  array<string, mixed>  $parameters
-     */
-    public function upload(array $parameters): never
-    {
-        throw new Exception('Not implemented yet.');
-    }
-
-    /**
-     * Delete a file.
-     *
-     * @see https://beta.openai.com/docs/api-reference/files/delete
-     */
-    public function delete(string $file): never
-    {
-        throw new Exception('Not implemented yet.');
-    }
-
-    /**
-     * Returns information about a specific file.
-     *
-     * @see https://beta.openai.com/docs/api-reference/files/retrieve
-     *
-     * @return array<string, mixed>
-     */
-    public function retrieve(string $file): never
-    {
-        throw new Exception('Not implemented yet.');
-    }
-
-    /**
-     * Returns the contents of the specified file
-     *
-     * @see https://beta.openai.com/docs/api-reference/files/retrieve-content
-     */
-    public function download(string $file): never
-    {
-        throw new Exception('Not implemented yet.');
     }
 }
