9c8dbdd1e8eb94a6519ace5042c1c5903230a89d
Filip Stojkovski <filip.stojkovski13@gmail.com>
2022-12-28 10:37:33 +0100

    Naming fixed of ApiKey and variable names

--- src/OpenAI.php
+++ src/OpenAI.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use GuzzleHttp\Client as GuzzleClient;
 use OpenAI\Client;
 use OpenAI\Transporters\HttpTransporter;
-use OpenAI\ValueObjects\ApiToken;
+use OpenAI\ValueObjects\ApiKey;
 use OpenAI\ValueObjects\Transporter\BaseUri;
 use OpenAI\ValueObjects\Transporter\Headers;
 
@@ -14,13 +14,13 @@ final class OpenAI
     /**
      * Creates a new Open AI Client with the given API token.
      */
-    public static function client(string $apiToken, string $organization = null): Client
+    public static function client(string $apiKey, string $organization = null): Client
     {
-        $apiToken = ApiToken::from($apiToken);
+        $apiKey = ApiKey::from($apiKey);
 
         $baseUri = BaseUri::from('api.openai.com/v1');
 
-        $headers = Headers::withAuthorization($apiToken);
+        $headers = Headers::withAuthorization($apiKey);
 
         if ($organization !== null) {
             $headers = $headers->withOrganization($organization);
