6bc5f1e7568dfa5ea82910de27c932b0ced702ca
gehrisandro <sandrogehri@gmail.com>
2023-06-15 06:10:04 +0200

    Add test for Chat/CreateResponse faking with "function_call"

--- tests/Responses/Chat/CreateResponse.php
+++ tests/Responses/Chat/CreateResponse.php
@@ -71,3 +71,27 @@ test('fake with override', function () {
         ->message->content->toBe('Hi, there!')
         ->message->role->toBe('assistant');
 });
+
+test('fake with function call', function () {
+    $response = CreateResponse::fake([
+        'id' => 'chatcmpl-111',
+        'choices' => [
+            [
+                'message' => [
+                    'function_call' => [
+                        'name' => 'get_current_weather',
+                        'arguments' => "{\n  \"location\": \"Boston, MA\"\n}",
+                    ],
+                ],
+                'finish_reason' => 'function_call',
+            ],
+        ],
+    ]);
+
+    expect($response)
+        ->id->toBe('chatcmpl-111')
+        ->and($response->choices[0])
+        ->message->functionCall->name->toBe('get_current_weather')
+        ->message->functionCall->arguments->toBe("{\n  \"location\": \"Boston, MA\"\n}")
+        ->finishReason->toBe('function_call');
+});
