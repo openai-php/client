5a4edb0288194bc7974d04548384a5d781570837
gehrisandro <sandrogehri@gmail.com>
2023-08-22 10:26:15 +0200

    WIP: update rector config

--- rector.php
+++ rector.php
@@ -14,8 +14,6 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->skip([
-        __DIR__.'/src/Contracts/ResponseContract.php',
-        __DIR__.'/src/Contracts/ResponseMetaContract.php',
         __DIR__.'/src/Testing/ClientFake.php' => FinalizeClassesWithoutChildrenRector::class,
     ]);
 
