fec764ff1d3289e0451fea79fc3e8016095b7905
gehrisandro <sandrogehri@gmail.com>
2023-11-14 09:20:44 +0100

    Add revised_prompt property to Image/CreateResponseData

--- src/Responses/Images/CreateResponseData.php
+++ src/Responses/Images/CreateResponseData.php
@@ -8,31 +8,33 @@ use OpenAI\Contracts\ResponseContract;
 use OpenAI\Responses\Concerns\ArrayAccessible;
 
 /**
- * @implements ResponseContract<array{url: string}|array{b64_json: string}>
+ * @implements ResponseContract<array{url: string, revised_prompt?: string}|array{b64_json: string, revised_prompt?: string}>
  */
 final class CreateResponseData implements ResponseContract
 {
     /**
-     * @use ArrayAccessible<array{url: string}|array{b64_json: string}>
+     * @use ArrayAccessible<array{url: string, revised_prompt?: string}|array{b64_json: string, revised_prompt?: string}>
      */
     use ArrayAccessible;
 
     private function __construct(
-        public readonly string $url = '',
-        public readonly string $b64_json = '',
+        public readonly string $url,
+        public readonly string $b64_json,
+        public readonly ?string $revisedPrompt,
     ) {
     }
 
     /**
      * Acts as static factory, and returns a new Response instance.
      *
-     * @param  array{url?: string, b64_json?: string}  $attributes
+     * @param  array{url?: string, b64_json?: string, revised_prompt?: string}  $attributes
      */
     public static function from(array $attributes): self
     {
         return new self(
             $attributes['url'] ?? '',
             $attributes['b64_json'] ?? '',
+            $attributes['revised_prompt'] ?? null,
         );
     }
 
@@ -41,8 +43,14 @@ final class CreateResponseData implements ResponseContract
      */
     public function toArray(): array
     {
-        return $this->url !== '' && $this->url !== '0' ?
+        $data = $this->url !== '' && $this->url !== '0' ?
             ['url' => $this->url] :
             ['b64_json' => $this->b64_json];
+
+        if ($this->revisedPrompt !== null) {
+            $data['revised_prompt'] = $this->revisedPrompt;
+        }
+
+        return $data;
     }
 }
