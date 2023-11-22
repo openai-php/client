dabc9c3f6339730a88d0b1c1ae4b670372600edf
gehrisandro <sandrogehri@gmail.com>
2023-11-22 14:53:54 +0100

    Improve TranslationResponseSegment transient parameter with mutation testing

--- src/Resources/Audio.php
+++ src/Resources/Audio.php
@@ -56,7 +56,7 @@ final class Audio implements AudioContract
     {
         $payload = Payload::upload('audio/translations', $parameters);
 
-        /** @var Response<array{task: ?string, language: ?string, duration: ?float, segments: array<int, array{id: int, seek: int, start: float, end: float, text: string, tokens: array<int, int>, temperature: float, avg_logprob: float, compression_ratio: float, no_speech_prob: float, transient: bool}>, text: string}> $response */
+        /** @var Response<array{task: ?string, language: ?string, duration: ?float, segments: array<int, array{id: int, seek: int, start: float, end: float, text: string, tokens: array<int, int>, temperature: float, avg_logprob: float, compression_ratio: float, no_speech_prob: float, transient?: bool}>, text: string}> $response */
         $response = $this->transporter->requestObject($payload);
 
         return TranslationResponse::from($response->data(), $response->meta());
--- tests/Responses/Audio/TranslationResponseSegment.php
+++ tests/Responses/Audio/TranslationResponseSegment.php
@@ -28,3 +28,29 @@ test('to array', function () {
     expect($result->toArray())
         ->toBe(audioTranslationVerboseJson()['segments'][0]);
 });
+
+test('transient parameter may not returned', function () {
+    $data = audioTranslationVerboseJson()['segments'][0];
+    unset($data['transient']);
+
+    $result = TranslationResponseSegment::from($data);
+
+    expect($result)
+        ->toBeInstanceOf(TranslationResponseSegment::class)
+        ->id->toBe(0)
+        ->seek->toBe(0)
+        ->start->toBe(0.0)
+        ->end->toBe(4.0)
+        ->text->toBe(' Hello, how are you?')
+        ->tokens->toBeArray()
+        ->tokens->toHaveCount(8)
+        ->tokens->toBe([50364, 2425, 11, 577, 366, 291, 30, 50564])
+        ->temperature->toBe(0.0)
+        ->avgLogprob->toBe(-0.45045216878255206)
+        ->compressionRatio->toBe(0.7037037037037037)
+        ->noSpeechProb->toBe(0.1076972484588623)
+        ->transient->toBeNull();
+
+    expect($result->toArray())
+        ->toBe($data);
+});
