36dd3a04af6137980b1ae0524eb21c3ea5bf1a59
Elodie Jean-Joseph <elodie.irdor@gmail.com>
2023-11-29 15:19:37 +1300

    Add test resources for Assistants and Threads

---
+++ src/Testing/Resources/AssistantsFilesTestResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OpenAI\Testing\Resources;
+
+use OpenAI\Contracts\Resources\AssistantsFilesContract;
+use OpenAI\Resources\AssistantsFiles;
+use OpenAI\Responses\Assistants\Files\AssistantFileDeleteResponse;
+use OpenAI\Responses\Assistants\Files\AssistantFileListResponse;
+use OpenAI\Responses\Assistants\Files\AssistantFileResponse;
+use OpenAI\Testing\Resources\Concerns\Testable;
+
+final class AssistantsFilesTestResource implements AssistantsFilesContract
+{
+    use Testable;
+
+    public function resource(): string
+    {
+        return AssistantsFiles::class;
+    }
+
+    public function create(string $assistantId, array $parameters): AssistantFileResponse
+    {
+        return $this->record(__FUNCTION__, $assistantId, $parameters);
+    }
+
+    public function retrieve(string $assistantId, string $fileId): AssistantFileResponse
+    {
+        return $this->record(__FUNCTION__, $assistantId, $fileId);
+    }
+
+    public function delete(string $assistantId, string $fileId): AssistantFileDeleteResponse
+    {
+        return $this->record(__FUNCTION__, $assistantId, $fileId);
+    }
+
+    public function list(string $assistantId, array $parameters = []): AssistantFileListResponse
+    {
+        return $this->record(__FUNCTION__, $assistantId, $parameters);
+    }
+}
---
+++ src/Testing/Resources/ThreadsRunsStepsTestResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OpenAI\Testing\Resources;
+
+use OpenAI\Contracts\Resources\ThreadsRunsStepsContract;
+use OpenAI\Resources\ThreadsRunsSteps;
+use OpenAI\Responses\Threads\Runs\Steps\ThreadRunStepListResponse;
+use OpenAI\Responses\Threads\Runs\Steps\ThreadRunStepResponse;
+use OpenAI\Testing\Resources\Concerns\Testable;
+
+class ThreadsRunsStepsTestResource implements ThreadsRunsStepsContract
+{
+    use Testable;
+
+    public function resource(): string
+    {
+        return ThreadsRunsSteps::class;
+    }
+
+    public function retrieve(string $threadId, string $runId, string $stepId): ThreadRunStepResponse
+    {
+        return $this->record(__FUNCTION__, $threadId, $runId, $stepId);
+    }
+
+    public function list(string $threadId, string $runId, array $parameters = []): ThreadRunStepListResponse
+    {
+        return $this->record(__FUNCTION__, $threadId, $runId, $parameters);
+    }
+}
