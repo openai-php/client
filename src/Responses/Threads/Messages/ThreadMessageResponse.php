d7a346ec460a4b852238ad236578c1782734dd38
Marlin Forbes <marlinf@datashaman.com>
2024-08-25 04:21:37 +0200

    Fix attachments error

--- src/Responses/Threads/Messages/ThreadMessageResponse.php
+++ src/Responses/Threads/Messages/ThreadMessageResponse.php
@@ -61,7 +61,7 @@ final class ThreadMessageResponse implements ResponseContract, ResponseHasMetaIn
 
         $attachments = array_map(
             fn (array $attachment): ThreadMessageResponseAttachment => ThreadMessageResponseAttachment::from($attachment),
-            $attributes['attachments']
+            $attributes['attachments'] ?? []
         );
 
         return new self(
