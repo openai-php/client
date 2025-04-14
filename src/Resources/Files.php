8ec4b1eddb23ec0fe8bf54ac2643c771b59b6b33
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-04-14 06:11:40 -0400

    chore: add files.list parameter support to contract/tests

--- src/Resources/Files.php
+++ src/Resources/Files.php
@@ -20,12 +20,10 @@ final class Files implements FilesContract
      * Returns a list of files that belong to the user's organization.
      *
      * @see https://platform.openai.com/docs/api-reference/files/list
-     *
-     * @param  array<string, mixed>  $parameters
      */
     public function list(array $parameters = []): ListResponse
     {
-        $payload = Payload::list('files', parameters: $parameters);
+        $payload = Payload::list('files', $parameters);
 
         /** @var Response<array{object: string, data: array<int, array{id: string, object: string, created_at: int, bytes: ?int, filename: string, purpose: string, status: string, status_details: array<array-key, mixed>|string|null}>}> $response */
         $response = $this->transporter->requestObject($payload);
