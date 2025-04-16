d8ce1a6379471f9d5ca33fb230112ff24630baab
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-04-15 20:24:17 -0400

    OutputMessage

--- src/Responses/Responses/Output/OutputMessage.php
+++ src/Responses/Responses/Output/OutputMessage.php
@@ -4,33 +4,71 @@ declare(strict_types=1);
 
 namespace OpenAI\Responses\Responses\Output;
 
-final class OutputMessage
+use OpenAI\Contracts\ResponseContract;
+use OpenAI\Responses\Concerns\ArrayAccessible;
+use OpenAI\Testing\Responses\Concerns\Fakeable;
+
+/**
+ * @implements ResponseContract<array{content: array<int, array{annotations: array<int, array{file_id: string, index: int, type: 'file_citation'}|array{file_id: string, index: int, type: 'file_path'}|array{end_index: int, start_index: int, title: string, type: 'url_citation', url: string}>, text: string, type: 'output_text'}|array{refusal: string, type: 'refusal'}>, id: string, role: string, status: 'in_progress'|'completed'|'incomplete', type: 'message'}>
+ */
+final class OutputMessage implements ResponseContract
 {
     /**
-     * @param  array<int, OutputMessage>  $outputs
+     * @use ArrayAccessible<array{content: array<int, array{annotations: array<int, array{file_id: string, index: int, type: 'file_citation'}|array{file_id: string, index: int, type: 'file_path'}|array{end_index: int, start_index: int, title: string, type: 'url_citation', url: string}>, text: string, type: 'output_text'}|array{refusal: string, type: 'refusal'}>, id: string, role: string, status: 'in_progress'|'completed'|'incomplete', type: 'message'}>
+     */
+    use ArrayAccessible;
+
+    use Fakeable;
+
+    /**
+     * @param array<int, OutputMessageContentOutputText|OutputMessageContentRefusal>  $content
+     * @param 'in_progress'|'completed'|'incomplete'  $status
+     * @param 'message' $type
      */
     private function __construct(
-        public readonly array $outputs,
+        public readonly array $content,
+        public readonly string $id,
+        public readonly string $role,
+        public readonly string $status,
+        public readonly string $type,
     ) {}
 
     /**
-     * @param  array{reasoning_tokens: int}  $attributes
+     * @param  array{content: array<int, array{annotations: array<int, array{file_id: string, index: int, type: 'file_citation'}|array{file_id: string, index: int, type: 'file_path'}|array{end_index: int, start_index: int, title: string, type: 'url_citation', url: string}>, text: string, type: 'output_text'}|array{refusal: string, type: 'refusal'}>, id: string, role: string, status: 'in_progress'|'completed'|'incomplete', type: 'message'}  $attributes
      */
     public static function from(array $attributes): self
     {
+        $content = array_map(
+            fn (array $item): OutputMessageContentOutputText|OutputMessageContentRefusal => match ($item['type']) {
+                'output_text' => OutputMessageContentOutputText::from($item),
+                'refusal' => OutputMessageContentRefusal::from($item),
+            },
+            $attributes['content'],
+        );
+
         return new self(
-            $attributes['reasoning_tokens'],
+            $content,
+            $attributes['id'],
+            $attributes['role'],
+            $attributes['status'],
+            $attributes['type'],
         );
     }
 
     /**
-     * @return array{reasoning_tokens: int}
+     * {@inheritDoc}
      */
     public function toArray(): array
     {
-        return array_map(
-            fn (OutputMessage $output): array => $output->toArray(),
-            $this->outputs,
-        );
+        return [
+            'content' => array_map(
+                fn (OutputMessageContentOutputText|OutputMessageContentRefusal $item): array => $item->toArray(),
+                $this->content,
+            ),
+            'id' => $this->id,
+            'role' => $this->role,
+            'status' => $this->status,
+            'type' => $this->type,
+        ];
     }
 }
