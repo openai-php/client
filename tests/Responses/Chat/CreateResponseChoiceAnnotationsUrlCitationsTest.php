151562a709d0e7e8cf0f3a80c3707399fbc068b6
Saraphoo <sarah.riley@sourcetoad.com>
2025-04-24 17:05:02 -0400

    Test CreateResponseChoiceAnnotationsUrlCitationsTest.php

--- tests/Responses/Chat/CreateResponseChoiceAnnotationsUrlCitationsTest.php
+++ tests/Responses/Chat/CreateResponseChoiceAnnotationsUrlCitationsTest.php
@@ -1,5 +1,20 @@
 <?php
 
-it('$END$', function () {
+use OpenAI\Responses\Chat\CreateResponseChoiceAnnotationsUrlCitations;
 
+it('from', function () {
+    $result = CreateResponseChoiceAnnotationsUrlCitations::from(chatCompletionWithAnnotations()['choices'][0]['annotations']['url_citation'][0]);
+
+    expect($result)
+        ->endIndex->toBe(5)
+        ->startIndex->toBe(0)
+        ->title->toBe('Hello')
+        ->url->toBe('https://example.com');
+});
+
+test('to array', function () {
+    $result = CreateResponseChoiceAnnotationsUrlCitations::from(chatCompletionWithAnnotations()['choices'][0]['annotations']['url_citation'][0]);
+
+    expect($result->toArray())
+        ->toBe(chatCompletionWithAnnotations()['choices'][0]['annotations']['url_citation'][0]);
 });
