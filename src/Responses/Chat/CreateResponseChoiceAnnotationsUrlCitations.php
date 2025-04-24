bf46c96844bcc3e72c45067eba2e41e188bdb464
Saraphoo <sarah.riley@sourcetoad.com>
2025-04-24 13:17:26 -0400

    CreateResponseChoiceAnnotaionUrlCitations class

--- src/Responses/Chat/CreateResponseChoiceAnnotationsUrlCitations.php
+++ src/Responses/Chat/CreateResponseChoiceAnnotationsUrlCitations.php
@@ -2,22 +2,38 @@
 
 namespace OpenAI\Responses\Chat;
 
-final class CreateResponseChoiceAnnotationsContent
+final class CreateResponseChoiceAnnotationsUrlCitations
 {
     public function __construct(
-    ) {
+        public readonly int $endIndex,
+        public readonly int $startIndex,
+        public readonly string $title,
+        public readonly string $url,
+    ) {}
 
-    }
-
+    /**
+     * @param array{start_index: int, end_index: int, title: string, url: string} $attributes
+     */
     public static function from(array $attributes): self
     {
         return new self(
+            $attributes['start_index'],
+            $attributes['end_index'],
+            $attributes['title'],
+            $attributes['url'],
         );
     }
 
+    /**
+     * @return array{start_index: int, end_index: int, title: string, url: string}
+     */
     public function toArray(): array
     {
         return [
+            'start_index' => $this->startIndex,
+            'end_index' => $this->endIndex,
+            'title' => $this->title,
+            'url' => $this->url,
         ];
     }
 }
