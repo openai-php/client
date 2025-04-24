17fa529a4cfb982dec2abfc6eae571193b8c1b78
Saraphoo <sarah.riley@sourcetoad.com>
2025-04-24 16:47:13 -0400

    test createResponseChoiceAnnotations

---
+++ tests/Responses/Chat/CreateResponseChoiceAnnotations.php
@@ -0,0 +1,23 @@
+<?php
+
+use OpenAI\Responses\Chat\CreateResponseChoiceAnnotations;
+use OpenAI\Responses\Chat\CreateResponseChoiceAnnotationsUrlCitations;
+
+
+it('from ', function () {
+    $result = CreateResponseChoiceAnnotations::from(chatCompletionWithAnnotations()['choices'][0]['annotations']);
+
+    expect($result)
+        ->toBeInstanceOf(CreateResponseChoiceAnnotations::class)
+        ->urlCitations->toBeArray()
+        ->urlCitations->toHaveLength(1)
+        ->urlCitations->each->toBeInstanceOf(CreateResponseChoiceAnnotationsUrlCitations::class);
+
+});
+
+test('to array', function () {
+    $result = CreateResponseChoiceAnnotations::from(chatCompletionWithAnnotations()['choices'][0]['annotations']);
+
+    expect($result->toArray())
+        ->toBe(chatCompletionWithAnnotations()['choices'][0]['annotations']);
+});
---
+++ tests/Responses/Chat/CreateResponseChoiceAnnotationsUrlCitationsTest.php
@@ -0,0 +1,5 @@
+<?php
+
+it('$END$', function () {
+
+});
