32d920e70d3b2c3df30d1d01a44e508972325055
Saraphoo <sarah.riley@sourcetoad.com>
2025-04-25 09:58:59 -0400

    Update test for CreateResponseChoiceAnnotations to pass

--- tests/Responses/Chat/CreateResponseChoiceAnnotations.php
+++ tests/Responses/Chat/CreateResponseChoiceAnnotations.php
@@ -4,19 +4,37 @@ use OpenAI\Responses\Chat\CreateResponseChoiceAnnotations;
 use OpenAI\Responses\Chat\CreateResponseChoiceAnnotationsUrlCitations;
 
 it('from ', function () {
-    $result = CreateResponseChoiceAnnotations::from(chatCompletionWithAnnotations()['choices'][0]['annotations']);
+    $annotation = chatCompletionWithAnnotations()['choices'][0]['message']['annotations'];
+
+    $attributes = [
+        'url_citation' => array_filter($annotation, fn ($item) => $item['type'] === 'url_citation'),
+    ];
+
+    $result = CreateResponseChoiceAnnotations::from($attributes);
 
     expect($result)
         ->toBeInstanceOf(CreateResponseChoiceAnnotations::class)
         ->urlCitations->toBeArray()
         ->urlCitations->toHaveLength(1)
         ->urlCitations->each->toBeInstanceOf(CreateResponseChoiceAnnotationsUrlCitations::class);
-
 });
+
 
 test('to array', function () {
-    $result = CreateResponseChoiceAnnotations::from(chatCompletionWithAnnotations()['choices'][0]['annotations']);
+    $annotation = chatCompletionWithAnnotations()['choices'][0]['message']['annotations'][0];
+
+    $citation = ['url_citation' => [$annotation]];
+    $result = CreateResponseChoiceAnnotations::from($citation);
 
     expect($result->toArray())
-        ->toBe(chatCompletionWithAnnotations()['choices'][0]['annotations']);
+        ->toBe([
+            'type' => 'url_citation',
+            'url_citation' => [
+                'end_index' => 5,
+                'start_index' => 0,
+                'title' => 'Hello',
+                'url' => 'https://example.com',
+            ],
+        ]);
 });
+
