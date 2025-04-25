c494f1152b3674c6ae408b7ec230a89f74abfa1a
Saraphoo <sarah.riley@sourcetoad.com>
2025-04-24 21:03:13 -0400

    Move annotations out of createResponseChoice

--- src/Responses/Chat/CreateResponseChoice.php
+++ src/Responses/Chat/CreateResponseChoice.php
@@ -9,34 +9,31 @@ final class CreateResponseChoice
     private function __construct(
         public readonly int $index,
         public readonly CreateResponseMessage $message,
-        public readonly ?CreateResponseChoiceAnnotations $annotations,
         public readonly ?CreateResponseChoiceLogprobs $logprobs,
         public readonly ?string $finishReason,
     ) {}
 
     /**
-     * @param  array{index: int, message: array{role: string, content: ?string, function_call: ?array{name: string, arguments: string}, tool_calls: ?array<int, array{id: string, type: string, function: array{name: string, arguments: string}}>}, annotations?: ?array<int, array{type: "url_citation", url_citation: array{start_index: int, end_index: int, title: string, url: string}}> ,logprobs?: ?array{content: ?array<int, array{token: string, logprob: float, bytes: ?array<int, int>}>}, finish_reason: string|null}  $attributes
+     * @param  array{index: int, message: array{role: string, content: ?string,annotations?: ?array<int, array{type: "url_citation", url_citation: array{start_index: int, end_index: int, title: string, url: string}}>, function_call: ?array{name: string, arguments: string}, tool_calls: ?array<int, array{id: string, type: string, function: array{name: string, arguments: string}}>} ,logprobs?: ?array{content: ?array<int, array{token: string, logprob: float, bytes: ?array<int, int>}>}, finish_reason: string|null}  $attributes
      */
     public static function from(array $attributes): self
     {
         return new self(
             $attributes['index'],
             CreateResponseMessage::from($attributes['message']),
-            isset($attributes['annotations']) ? CreateResponseChoiceAnnotations::from($attributes['annotations']) : null,
             isset($attributes['logprobs']) ? CreateResponseChoiceLogprobs::from($attributes['logprobs']) : null,
             $attributes['finish_reason'] ?? null,
         );
     }
 
     /**
-     * @return array{index: int, message: array{role: string, content: string|null, function_call?: array{name: string, arguments: string}, tool_calls?: array<int, array{id: string, type: string, function: array{name: string, arguments: string}}>},annotations?: ?array<int, array{type: "url_citation", url_citation: array{start_index: int, end_index: int, title: string, url: string}}> , logprobs: ?array{content: ?array<int, array{token: string, logprob: float, bytes: ?array<int, int>}>}, finish_reason: string|null}
+     * @return array{index: int, message: array{role: string, content: string|null,annotations?: ?array<int, array{type: "url_citation", url_citation: array{start_index: int, end_index: int, title: string, url: string}}>, function_call?: array{name: string, arguments: string}, tool_calls?: array<int, array{id: string, type: string, function: array{name: string, arguments: string}}>}, logprobs: ?array{content: ?array<int, array{token: string, logprob: float, bytes: ?array<int, int>}>}, finish_reason: string|null}
      */
     public function toArray(): array
     {
         return [
             'index' => $this->index,
             'message' => $this->message->toArray(),
-            'annotations' => $this->annotations?->toArray(),
             'logprobs' => $this->logprobs?->toArray(),
             'finish_reason' => $this->finishReason,
         ];
