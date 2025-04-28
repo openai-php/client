f56e38e36f1ed0ac806f678b20de66f9c52e3909
Saraphoo <sarah.riley@sourcetoad.com>
2025-04-28 16:31:42 -0400

    Return annotations from createResponseChoiceAnnotations class to response message correctly

--- src/Responses/Chat/CreateResponseChoiceAnnotations.php
+++ src/Responses/Chat/CreateResponseChoiceAnnotations.php
@@ -5,14 +5,14 @@ namespace OpenAI\Responses\Chat;
 final class CreateResponseChoiceAnnotations
 {
     /**
-     * @param  ?array<int, CreateResponseChoiceAnnotationsUrlCitations>  $citations
+     * @param  ?array<int, CreateResponseChoiceAnnotationsUrlCitations>  $annotations
      */
     public function __construct(
-        public readonly ?array $citations,
+        public readonly ?array $annotations,
     ) {}
 
     /**
-     * @param array{type: 'url_citation', url_citation: array{start_index: int, end_index: int, title: string, url: string}} $attributes
+     * @param  array{type: 'url_citation', url_citation: array{ end_index: int, start_index: int, title: string, url: string}}  $attributes
      */
     public static function from(array $attributes): self
     {
@@ -26,19 +26,16 @@ final class CreateResponseChoiceAnnotations
     }
 
     /**
-     *  @return array{annotations: array<int, array{type: string, url_citation: array{start_index: int, end_index: int, title: string, url: string}}>}
+     * @return array{annotations: array<int, array{type: string, url_citation: array{end_index: int, start_index: int, title: string, url: string}}>}
      */
     public function toArray(): array
     {
-        return [
-            'annotations' => array_map(
-                fn(CreateResponseChoiceAnnotationsUrlCitations $citation): array => [
-                    'type' => 'url_citation',
-                    'url_citation' => $citation->toArray(),
-                ],
-                $this->citations
-            ),
-        ];
+        return array_map(
+            fn (CreateResponseChoiceAnnotationsUrlCitations $citation): array => [
+                'type' => 'url_citation',
+                'url_citation' => $citation->toArray(),
+            ],
+            $this->annotations ?? null
+        );
     }
-
 }
