db942f978c0585bc5bafeefc51123a66fe31e054
Saraphoo <sarah.riley@sourcetoad.com>
2025-04-28 16:34:31 -0400

    expect annotations

--- tests/Responses/Chat/CreateResponseChoiceAnnotations.php
+++ tests/Responses/Chat/CreateResponseChoiceAnnotations.php
@@ -3,38 +3,30 @@
 use OpenAI\Responses\Chat\CreateResponseChoiceAnnotations;
 use OpenAI\Responses\Chat\CreateResponseChoiceAnnotationsUrlCitations;
 
-it('from ', function () {
-    $annotation = chatCompletionWithAnnotations()['choices'][0]['message']['annotations'];
-
-    $attributes = [
-        'url_citation' => array_filter($annotation, fn ($item) => $item['type'] === 'url_citation'),
-    ];
-
-    $result = CreateResponseChoiceAnnotations::from($attributes);
+it('from url_citation annotation', function () {
+    $result = CreateResponseChoiceAnnotations::from(chatCompletionWithAnnotations()['choices'][0]['message']['annotations'][0]);
 
     expect($result)
         ->toBeInstanceOf(CreateResponseChoiceAnnotations::class)
-        ->urlCitations->toBeArray()
-        ->urlCitations->toHaveLength(1)
-        ->urlCitations->each->toBeInstanceOf(CreateResponseChoiceAnnotationsUrlCitations::class);
+        ->annotations->toBeArray()
+        ->annotations->toHaveLength(1)
+        ->annotations->each->toBeInstanceOf(CreateResponseChoiceAnnotationsUrlCitations::class);
 });
 
-
 test('to array', function () {
-    $annotation = chatCompletionWithAnnotations()['choices'][0]['message']['annotations'][0];
-
-    $citation = ['url_citation' => [$annotation]];
-    $result = CreateResponseChoiceAnnotations::from($citation);
+    $result = CreateResponseChoiceAnnotations::from(chatCompletionWithAnnotations()['choices'][0]['message']['annotations'][0]);
 
     expect($result->toArray())
         ->toBe([
-            'type' => 'url_citation',
-            'url_citation' => [
-                'end_index' => 5,
-                'start_index' => 0,
-                'title' => 'Hello',
-                'url' => 'https://example.com',
+            [
+                'type' => 'url_citation',
+                'url_citation' => [
+                    'end_index' => 5,
+                    'start_index' => 0,
+                    'title' => 'Hello',
+                    'url' => 'https://example.com',
+                ],
             ],
+
         ]);
 });
-
