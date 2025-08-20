b00d14d62e50a3e0f534a0c02ee28cda683a6316
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-08-20 06:40:07 -0400

    feat: add fixtures

---
+++ src/Testing/Responses/Fixtures/Containers/CreateContainerFixture.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OpenAI\Testing\Responses\Fixtures\Containers;
+
+final class CreateContainerFixture
+{
+    public const ATTRIBUTES = [
+        'id' => 'container_abc123',
+        'object' => 'container',
+        'created_at' => 1690000000,
+        'status' => 'active',
+        'expires_after' => [
+            'anchor' => 'last_active_at',
+            'minutes' => 60,
+        ],
+        'last_active_at' => 1690001000,
+        'name' => 'Test Container',
+    ];
+}
