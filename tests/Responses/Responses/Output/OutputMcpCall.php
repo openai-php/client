87f52477caae31781e6a337b8a814766beb8e8ee
Connor Tumbleson <connor@sourcetoad.com>
2025-08-27 06:31:37 -0400

    fix(OpenAI): remove unneeded import

--- tests/Responses/Responses/Output/OutputMcpCall.php
+++ tests/Responses/Responses/Output/OutputMcpCall.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__.'/../../../Fixtures/Responses.php';
-
 use OpenAI\Responses\Responses\GenericResponseError;
 use OpenAI\Responses\Responses\McpGenericResponseError;
 use OpenAI\Responses\Responses\Output\OutputMcpCall;
