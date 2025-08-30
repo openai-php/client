8f8414adb3bc92a694d7f72e542788fc8da9ab4f
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2025-08-30 07:09:26 -0400

    fix(OpenAI): Add missing properties to ListInputItems call (#668)
    * fix(OpenAI): add missing properties to ListItems for Response API
    
    * test(OpenAI): augment tests for ListInputItems

--- src/Responses/Responses/ListInputItems.php
+++ src/Responses/Responses/ListInputItems.php
@@ -12,10 +12,16 @@ use OpenAI\Responses\Meta\MetaInformation;
 use OpenAI\Responses\Responses\Input\ComputerToolCallOutput;
 use OpenAI\Responses\Responses\Input\FunctionToolCallOutput;
 use OpenAI\Responses\Responses\Input\InputMessage;
+use OpenAI\Responses\Responses\Output\OutputCodeInterpreterToolCall;
 use OpenAI\Responses\Responses\Output\OutputComputerToolCall;
 use OpenAI\Responses\Responses\Output\OutputFileSearchToolCall;
 use OpenAI\Responses\Responses\Output\OutputFunctionToolCall;
+use OpenAI\Responses\Responses\Output\OutputImageGenerationToolCall;
+use OpenAI\Responses\Responses\Output\OutputMcpApprovalRequest;
+use OpenAI\Responses\Responses\Output\OutputMcpCall;
+use OpenAI\Responses\Responses\Output\OutputMcpListTools;
 use OpenAI\Responses\Responses\Output\OutputMessage;
+use OpenAI\Responses\Responses\Output\OutputReasoning;
 use OpenAI\Responses\Responses\Output\OutputWebSearchToolCall;
 use OpenAI\Testing\Responses\Concerns\Fakeable;
 
@@ -28,8 +34,14 @@ use OpenAI\Testing\Responses\Concerns\Fakeable;
  * @phpstan-import-type OutputWebSearchToolCallType from OutputWebSearchToolCall
  * @phpstan-import-type OutputFunctionToolCallType from OutputFunctionToolCall
  * @phpstan-import-type FunctionToolCallOutputType from FunctionToolCallOutput
+ * @phpstan-import-type OutputReasoningType from OutputReasoning
+ * @phpstan-import-type OutputMcpListToolsType from OutputMcpListTools
+ * @phpstan-import-type OutputMcpApprovalRequestType from OutputMcpApprovalRequest
+ * @phpstan-import-type OutputMcpCallType from OutputMcpCall
+ * @phpstan-import-type OutputImageGenerationToolCallType from OutputImageGenerationToolCall
+ * @phpstan-import-type OutputCodeInterpreterToolCallType from OutputCodeInterpreterToolCall
  *
- * @phpstan-type ListInputItemsType array{data: array<int, InputMessageType|OutputMessageType|OutputFileSearchToolCallType|OutputComputerToolCallType|ComputerToolCallOutputType|OutputWebSearchToolCallType|OutputFunctionToolCallType|FunctionToolCallOutputType>, first_id: string, has_more: bool, last_id: string, object: 'list'}
+ * @phpstan-type ListInputItemsType array{data: array<int, InputMessageType|OutputMessageType|OutputFileSearchToolCallType|OutputComputerToolCallType|ComputerToolCallOutputType|OutputWebSearchToolCallType|OutputFunctionToolCallType|FunctionToolCallOutputType|OutputReasoningType|OutputMcpListToolsType|OutputMcpApprovalRequestType|OutputMcpCallType|OutputImageGenerationToolCallType|OutputCodeInterpreterToolCallType>, first_id: string, has_more: bool, last_id: string, object: 'list'}
  *
  * @implements ResponseContract<ListInputItemsType>
  */
@@ -42,7 +54,7 @@ final class ListInputItems implements ResponseContract, ResponseHasMetaInformati
     use HasMetaInformation;
 
     /**
-     * @param  array<int, InputMessage|OutputMessage|OutputFileSearchToolCall|OutputComputerToolCall|ComputerToolCallOutput|OutputWebSearchToolCall|OutputFunctionToolCall|FunctionToolCallOutput>  $data
+     * @param  array<int, InputMessage|OutputMessage|OutputFileSearchToolCall|OutputComputerToolCall|ComputerToolCallOutput|OutputWebSearchToolCall|OutputFunctionToolCall|FunctionToolCallOutput|OutputReasoning|OutputMcpListTools|OutputMcpApprovalRequest|OutputMcpCall|OutputImageGenerationToolCall|OutputCodeInterpreterToolCall>  $data
      * @param  'list'  $object
      */
     private function __construct(
@@ -60,7 +72,7 @@ final class ListInputItems implements ResponseContract, ResponseHasMetaInformati
     public static function from(array $attributes, MetaInformation $meta): self
     {
         $data = array_map(
-            fn (array $item): InputMessage|OutputMessage|OutputFileSearchToolCall|OutputComputerToolCall|ComputerToolCallOutput|OutputWebSearchToolCall|OutputFunctionToolCall|FunctionToolCallOutput => match ($item['type']) {
+            fn (array $item): InputMessage|OutputMessage|OutputFileSearchToolCall|OutputComputerToolCall|ComputerToolCallOutput|OutputWebSearchToolCall|OutputFunctionToolCall|FunctionToolCallOutput|OutputReasoning|OutputMcpListTools|OutputMcpApprovalRequest|OutputMcpCall|OutputImageGenerationToolCall|OutputCodeInterpreterToolCall => match ($item['type']) {
                 'message' => $item['role'] === 'assistant' ? OutputMessage::from($item) : InputMessage::from($item),
                 'file_search_call' => OutputFileSearchToolCall::from($item),
                 'function_call' => OutputFunctionToolCall::from($item),
@@ -68,6 +80,12 @@ final class ListInputItems implements ResponseContract, ResponseHasMetaInformati
                 'web_search_call' => OutputWebSearchToolCall::from($item),
                 'computer_call' => OutputComputerToolCall::from($item),
                 'computer_call_output' => ComputerToolCallOutput::from($item),
+                'reasoning' => OutputReasoning::from($item),
+                'mcp_list_tools' => OutputMcpListTools::from($item),
+                'mcp_approval_request' => OutputMcpApprovalRequest::from($item),
+                'mcp_call' => OutputMcpCall::from($item),
+                'image_generation_call' => OutputImageGenerationToolCall::from($item),
+                'code_interpreter_call' => OutputCodeInterpreterToolCall::from($item),
             },
             $attributes['data'],
         );
@@ -90,7 +108,7 @@ final class ListInputItems implements ResponseContract, ResponseHasMetaInformati
         return [
             'object' => $this->object,
             'data' => array_map(
-                fn (InputMessage|OutputMessage|OutputFileSearchToolCall|OutputComputerToolCall|ComputerToolCallOutput|OutputWebSearchToolCall|OutputFunctionToolCall|FunctionToolCallOutput $item): array => $item->toArray(),
+                fn (InputMessage|OutputMessage|OutputFileSearchToolCall|OutputFunctionToolCall|FunctionToolCallOutput|OutputWebSearchToolCall|OutputComputerToolCall|ComputerToolCallOutput|OutputReasoning|OutputMcpListTools|OutputMcpApprovalRequest|OutputMcpCall|OutputImageGenerationToolCall|OutputCodeInterpreterToolCall $item): array => $item->toArray(),
                 $this->data,
             ),
             'first_id' => $this->firstId,
