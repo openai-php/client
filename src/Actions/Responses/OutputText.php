22c554f574c04fe313e41e2330f43d71b61a8665
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-09-02 06:21:28 -0400

    chore: unify types on output text for less duplication

--- src/Actions/Responses/OutputText.php
+++ src/Actions/Responses/OutputText.php
@@ -4,28 +4,18 @@ declare(strict_types=1);
 
 namespace OpenAI\Actions\Responses;
 
-use OpenAI\Responses\Responses\Output\OutputCodeInterpreterToolCall;
-use OpenAI\Responses\Responses\Output\OutputComputerToolCall;
-use OpenAI\Responses\Responses\Output\OutputFileSearchToolCall;
-use OpenAI\Responses\Responses\Output\OutputFunctionToolCall;
-use OpenAI\Responses\Responses\Output\OutputImageGenerationToolCall;
-use OpenAI\Responses\Responses\Output\OutputMcpApprovalRequest;
-use OpenAI\Responses\Responses\Output\OutputMcpCall;
-use OpenAI\Responses\Responses\Output\OutputMcpListTools;
 use OpenAI\Responses\Responses\Output\OutputMessage;
 use OpenAI\Responses\Responses\Output\OutputMessageContentOutputText;
-use OpenAI\Responses\Responses\Output\OutputReasoning;
-use OpenAI\Responses\Responses\Output\OutputWebSearchToolCall;
 
 /**
  * An SDK-only property (output_text) that concatenates all text content from output messages.
  *
- * @phpstan-type ResponseOutputTextTypes array<int, OutputMessage|OutputComputerToolCall|OutputFileSearchToolCall|OutputWebSearchToolCall|OutputFunctionToolCall|OutputReasoning|OutputMcpListTools|OutputMcpApprovalRequest|OutputMcpCall|OutputImageGenerationToolCall|OutputCodeInterpreterToolCall>
+ * @phpstan-import-type ResponseOutputObjectReturnType from OutputObjects
  */
 final class OutputText
 {
     /**
-     * @param  ResponseOutputTextTypes  $outputItems
+     * @param  ResponseOutputObjectReturnType  $outputItems
      */
     public static function parse(array $outputItems): ?string
     {
