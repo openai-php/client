0094710f3faceb811862d7f35dcebf82964ecf8e
Bob Olde Hampsink <bob@robuust.digital>
2025-11-13 09:01:15 +0100

    Allow setting stream parameter to different value

--- src/Resources/Concerns/Streamable.php
+++ src/Resources/Concerns/Streamable.php
@@ -8,14 +8,16 @@ trait Streamable
 {
     /**
      * @param  array<string, mixed>  $parameters
+     * @param  string                $fallbackFunction
+     * @param  mixed                 $streamValue
      */
-    private function ensureNotStreamed(array $parameters, string $fallbackFunction = 'createStreamed'): void
+    private function ensureNotStreamed(array $parameters, string $fallbackFunction = 'createStreamed', mixed $streamValue = true): void
     {
         if (! isset($parameters['stream'])) {
             return;
         }
 
-        if ($parameters['stream'] !== true) {
+        if ($parameters['stream'] !== $streamValue) {
             return;
         }
 
@@ -26,11 +28,12 @@ trait Streamable
      * Set the stream parameter to true.
      *
      * @param  array<string, mixed>  $parameters
+     * @param  mixed           $streamValue
      * @return array<string, mixed>
      */
-    private function setStreamParameter(array $parameters): array
+    private function setStreamParameter(array $parameters, mixed $streamValue = true): array
     {
-        $parameters['stream'] = true;
+        $parameters['stream'] = $streamValue;
 
         return $parameters;
     }
